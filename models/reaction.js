e957729c4f2a3d4409420042d504de207b7f055c
Megan Wright <meganmwright2018@gmail.com>
2023-12-16 21:09:22 -0600

    Schema for Reaction + comments

---
+++ models/reaction.js
@@ -0,0 +1,35 @@
+// Importing Schema and model from mongoose 
+const { Schema, Types } = require('mongoose');
+
+// Declaring Schema definition for reaction subdocument
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => timestamp.toDateString(), // Example getter to format the date
+    },
+  },
+  {
+    toJSON: {
+      getters: true, // Ensuring any getters defined are applied
+    },
+    id: false, // Preventing the virtual _id field from being added
+  }
+);
+
+// Exporting model to be used elsewhere
+module.exports = reactionSchema;
