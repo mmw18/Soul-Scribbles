d448f4e02d63fb1c92c79e4dc7723f28ed76e54c
Megan Wright <meganmwright2018@gmail.com>
2023-12-16 22:01:48 -0600

    API routes for user + commenting

---
+++ routes/userRoutes.js
@@ -0,0 +1,104 @@
+// Importing user.js model and express
+const router = require('express').Router();
+const User = require('../models/user'); 
+
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find().populate('thoughts').populate('friends');
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET a single user by its _id and populated thought and friend data
+router.get('/:userId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
+        if (!user) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// POST a new user
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// PUT to update a user by its _id
+router.put('/:userId', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+        if (!updatedUser) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE to remove user by its _id
+router.delete('/:userId', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.userId);
+        if (!user) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json({ message: 'User deleted successfully!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// POST to add a new friend to a user's friend list
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $addToSet: { friends: req.params.friendId } }, // Prevents duplicate entries
+            { new: true }
+        );
+        if (!user) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE to remove a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        );
+        if (!user) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Exporting module to be used elsewhere
+module.exports = router;
