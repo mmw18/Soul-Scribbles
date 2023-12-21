037ce4847d2cd6e28499d99bd106352da59a7dcf
Megan Wright <meganmwright2018@gmail.com>
2023-12-21 14:23:16 -0600

    Correcting thought route file

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,30 @@
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+} = require('../controllers/thoughtController');
 
+router.route('/')
+  .get(getAllThoughts)
+  .post(createThought);
 
+router.route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
+router.route('/:thoughtId/reactions')
+  .post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
+
+module.exports = router;
 
 
 // // Importing thought.js,user.js, and express
