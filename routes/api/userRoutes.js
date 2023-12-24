1ad6c8bb64d1fcfe929a2aa8d2f8d352041ae38b
Megan Wright <meganmwright2018@gmail.com>
2023-12-24 12:43:26 -0600

    cleaning up user route, connecting code moved over to controller file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,108 +1,28 @@
-// Importing user.js model and express
 const router = require('express').Router();
-const User = require('../../models/user'); 
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
 
-// GET all users
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find().populate('thoughts').populate('friends');
-        res.json(users);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// /api/users
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
 
-// GET a single user by its _id and populated thought and friend data
-router.get('/:userId', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
-        if (!user) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-        }
-        res.json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
-// POST a new user
-router.post('/', async (req, res) => {
-    try {
-        const newUser = await User.create(req.body);
-        res.json(newUser);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
 
-// PUT to update a user by its _id
-router.put('/:userId', async (req, res) => {
-    try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
-        if (!updatedUser) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-        }
-        res.json(updatedUser);
-    } catch (err) {
-        if (err.name === 'CastError') {
-            res.status(400).json({ message: 'Invalid user id format' });
-        } else {
-            res.status(500).json(err);
-        }
-    }
-});
-
-// DELETE to remove user by its _id
-router.delete('/:userId', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.userId);
-        if (!user) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-        }
-        res.json({ message: 'User deleted successfully!' });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// POST to add a new friend to a user's friend list
-router.post('/:userId/friends/:friendId', async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(
-            req.params.userId,
-            { $addToSet: { friends: req.params.friendId } }, // Prevents duplicate entries
-            { new: true }
-        );
-        if (!user) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-        }
-        res.json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// DELETE to remove a friend from a user's friend list
-router.delete('/:userId/friends/:friendId', async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(
-            req.params.userId,
-            { $pull: { friends: req.params.friendId } },
-            { new: true }
-        );
-        if (!user) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-        }
-        res.json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// Exporting module to be used elsewhere
 module.exports = router;
